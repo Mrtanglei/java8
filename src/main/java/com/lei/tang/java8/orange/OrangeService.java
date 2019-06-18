873dbc0d4e361758ca41bf27d75941157bdbf45b
tanglei <lei.tang@icepointcloud.com>
2019-06-18 14:12:17 +0800

    Lambda表达式

--- src/main/java/com/lei/tang/java8/orange/OrangeService.java
+++ src/main/java/com/lei/tang/java8/orange/OrangeService.java
@@ -4,17 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.lei.tang.java8.domain.Orange;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * @author tanglei
  * @date 2019/5/30
  */
+@Slf4j
 public class OrangeService {
 
-    public List<Orange> filterOranges(List<Orange> source,OrangePredicate p,Object obj){
+    public List<Orange> filterOranges(List<Orange> source, OrangePredicate p, Object obj) {
         List<Orange> result = new ArrayList<>();
-        for (Orange orange:source){
-            if(p.fileter(orange, obj)){
+        for (Orange orange : source) {
+            if (p.fileter(orange, obj)) {
                 result.add(orange);
             }
         }
@@ -22,7 +24,7 @@ public class OrangeService {
     }
 
 
-    public List<Orange> filterOranges(List<Orange> source){
+    public List<Orange> filterOranges(List<Orange> source) {
         return filterOranges(source, new OrangePredicate() {
             @Override
             public boolean fileter(Orange orange, Object obj) {
@@ -31,18 +33,21 @@ public class OrangeService {
         }, 100);
     }
 
-    public List<Orange> filterOrangesFromLambda(List<Orange> source){
+    public List<Orange> filterOrangesFromLambda(List<Orange> source) {
         List<Orange> oranges = filterOranges(source, (Orange orange, Object obj) -> (Double) obj < orange.getWeight()
                 , 100);
         return oranges;
     }
 
-    public List<Orange> filter(List<Orange> oranges){
-        return FilterUtils.filter(oranges, new FilterUtils.Predicate<Orange>() {
-            @Override
-            public boolean filter(Orange orange) {
-                return orange.getWeight() > 100 && "green".equals(orange.getColor());
-            }
-        });
+    public List<Orange> filter(List<Orange> oranges) {
+        //使用Lambda表达式
+        return FilterUtils.filter(oranges, (orange) -> orange.getWeight() > 100 && "green".equals(orange.getColor()));
+        //未使用
+        //        return FilterUtils.filter(oranges, new FilterUtils.Predicate<Orange>() {
+        //            @Override
+        //            public boolean filter(Orange orange) {
+        //                return orange.getWeight() > 100 && "green".equals(orange.getColor());
+        //            }
+        //        });
     }
 }

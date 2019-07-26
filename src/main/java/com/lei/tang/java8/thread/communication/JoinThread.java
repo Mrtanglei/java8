6a8c7ccbb1dbc1bd95a1b7e081fb77f522da47b2
tanglei <lei.tang@icepointcloud.com>
2019-07-26 08:53:39 +0800

    线程通信之join方法

---
+++ src/main/java/com/lei/tang/java8/thread/communication/JoinThread.java
@@ -0,0 +1,45 @@
+package com.lei.tang.java8.thread.communication;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author tanglei
+ * @date 2019/7/25
+ *
+ * Thread join方法使外线程等待到此线程接收继续执行
+ */
+@Slf4j
+public class JoinThread {
+
+    public static void join() throws InterruptedException {
+        long startTime = System.currentTimeMillis();
+        Thread threadA = new Thread(() -> {
+            try {
+                log.info("threadA start");
+                Thread.sleep(4000);
+                log.info("threadA end");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        Thread threadB = new Thread(() -> {
+            try {
+                threadA.join();
+                log.info("threadB start");
+                Thread.sleep(3000);
+                log.info("threadB end");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        threadA.start();
+        threadB.start();
+
+
+        threadB.join();
+        log.info("run time [{}]", startTime - System.currentTimeMillis());
+        log.info("main thread end");
+    }
+}
